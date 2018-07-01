4f55f79a4d535f5e0496a27eec3a8f515ae3a791
Yash Sompura <sompurayash@live.com>
2018-07-01 19:04:08 -0400

    [ADD][DESIGN] Driver, Rider and Logo

---
+++ app/src/main/java/harsh/keshwala/com/carpool/DriverLoginActivity.java
@@ -0,0 +1,29 @@
+package harsh.keshwala.com.carpool;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class DriverLoginActivity extends AppCompatActivity {
+
+    private TextView driverSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        driverSignUp = (TextView) findViewById(R.id.driverSignUpText);
+        driverSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DriverLoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/harsh/keshwala/com/carpool/RiderLoginActivity.java
@@ -0,0 +1,26 @@
+package harsh.keshwala.com.carpool;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RiderLoginActivity extends AppCompatActivity {
+
+    private TextView riderSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider_login);
+
+        riderSignUp = (TextView) findViewById(R.id.riderSignUpText);
+        riderSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RiderLoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
