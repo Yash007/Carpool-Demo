d5e0f4aa74d723da2e8a1e48b81152c0ea94c514
Yash Sompura <sompurayash@live.com>
2018-07-02 00:56:04 -0400

    [ADD][CODE] Driver login and Signup

---
+++ app/src/main/java/harsh/keshwala/com/carpool/Config.java
@@ -0,0 +1,9 @@
+package harsh.keshwala.com.carpool;
+
+public class Config {
+
+    public static final String URL = "http://carpool.cptrivedi.com/codebase/";
+
+    public static final String PREF_NAME = "carpool";
+
+}
